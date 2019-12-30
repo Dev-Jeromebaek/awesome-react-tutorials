928d94a344d8abb1ba7b2051864a102787ba579c
seungyeub <superior051@icloud.com>
2019-12-30 15:15:06 +0900

    feat: Add addon-viewport in storybook-uikit-tutorial-ts

--- storybook-uikit-tutorial-ts/.storybook/config.js
+++ storybook-uikit-tutorial-ts/.storybook/config.js
@@ -1,4 +1,12 @@
-import { configure } from "@storybook/react";
+import { configure, addParameters } from "@storybook/react";
 
 // automatically import all files ending in *.stories.js
 configure(require.context("../src", true, /\.stories\.(js|mdx|tsx)$/), module);
+
+// 
+addParameters({
+  viewport: {
+    viewports: newViewports, // newViewports would be an ViewportMap. (see below for examples)
+    defaultViewport: "someDefault"
+  }
+});
